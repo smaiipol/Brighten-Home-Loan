7bb5c14ec494894f49101b9f40541669cebb2409
huangmin <729732688@qq.com>
2018-05-13 19:15:48 +0800

    第二版
    优化移动端菜单，加入禁止滑动事件，以及首页倒数第3部分移动端超出省略号

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 jQuery(document).ready(function($){
+	/*------------ Fade plugin Start ------------*/
 	AOS.init();
+	/*------------ Fade plugin End --------------*/
+
+	/*------------ Hamburger Start ------------*/
 	$('.hamburger').click(function(){
 		$(this).toggleClass('is-active');
 		$('.menu-hide').toggleClass('on');
 		$('body').toggleClass('scroll');
+		if ($('body').hasClass('scroll')) {
+			stop();
+		} else {
+			move();
+		}
 	})
+	/*------------ Hamburger End --------------*/
+
+	/*------------ Faq section Start ------------*/
 	$('.faq-block').eq(0).addClass('opened');
 	$('.faq-block').not(':eq(0)').find('.faq-block-text').hide();
 	$('.circle-plus').on('click', function(){
@@ -13,13 +25,28 @@ jQuery(document).ready(function($){
 		$(this).toggleClass('opened');
 		$(this).find('.faq-block-text').slideToggle();
 	})
+	/*------------ Faq section End --------------*/
 })
 
 jQuery(window).scroll(function(event) {
+	/*------------ Header BG Start ------------*/
 	var windowScrollTop = jQuery(window).scrollTop();
 	if (windowScrollTop > 50) {
 		jQuery('header .nav').addClass('bg-white');
 	} else {
 		jQuery('header .nav').removeClass('bg-white');
 	}
+	/*------------ Header BG End --------------*/
 });
+
+/*------------ Disable Touch Event Start ------------*/
+var mo=function(e){e.preventDefault()};
+
+function stop(){
+    document.addEventListener("touchmove",mo,false);
+}
+
+function move(){
+    document.removeEventListener("touchmove",mo,false);
+}
+/*------------ Disable Touch Event End --------------*/
